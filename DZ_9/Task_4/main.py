cd40d2222c9fe97c7372a18507a7097d2e6f0a80
RomanBusygin <roman.alex.95@mail.ru>
2022-10-02 15:23:51 +0500

    Task_4

--- DZ_9/Task_4/main.py
+++ DZ_9/Task_4/main.py
@@ -4,11 +4,10 @@ bot = '5699740380:AAH8f6hNtBnCs_-fqN9e7WOB94FNBgkwJdA'
 city = input('Введите название города: ')
 url_text = f'https://wttr.in/{city}?M'
 url_png = f'https://wttr.in/{city}_0pM.png'
-weather_text = requests.get(url_text)
-weather_png = requests.get(url_png)
-print(weather_text.text)
+
+print(requests.get(url_text).text)
 
 with open(r'C:\Users\Roman\Desktop\GEEK\Block_2\Phyton\DZ\DZ_9\Task_4\imag.png', 'wb') as file:
-    file.write(weather_png.content)
+    file.write(requests.get(url_png).content)
     
 requests.get(f'https://api.telegram.org/bot{bot}/sendPhoto', params = dict(chat_id = '864101142'), files = {'photo': open('imag.png', 'rb')})
